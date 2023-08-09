23b1fd617991bbbb0420bcf4539f7899484ad082
craighicks13 <craighicks13@gmail.com>
2023-08-09 08:47:35 -0600

    feat: Update post description and episode page layout
    - Updated the description of a post in the public/post.json file to use HTML tags for better formatting and readability.
    - Modified the layout of the episode page in src/app/[episode]/page.tsx to display two buttons for different podcast platforms instead of three, and added icons for Spotify and Apple Podcasts.
    
    These changes improve the appearance and user experience of the post description and episode page.

--- src/app/[episode]/page.tsx
+++ src/app/[episode]/page.tsx
@@ -1,5 +1,6 @@
 import { Container } from '@/components/Container'
 import { FormattedDate } from '@/components/FormattedDate'
+import { Icons } from '@/components/Icons'
 import { Metadata, ResolvingMetadata } from 'next'
 import Image from 'next/image'
 
@@ -41,9 +42,15 @@ export default async function Episode({ params }: Props) {
                   date={date}
                   className="order-first font-mono text-sm leading-7 text-slate-500"
                 />
-                <div className="grid grid-cols-3 gap-3">
-                  <button className="btn btn-primary">Listen</button>
-                  <button className="btn btn-secondary">Listen</button>
+                <div className="grid grid-cols-2 gap-3">
+                  <button className="btn btn-primary">
+                    <Icons.spotify className="h-6 w-6" />
+                    Spotify
+                  </button>
+                  <button className="btn btn-secondary">
+                    <Icons.apple className="h-8 w-8" />
+                    Apple Podcasts
+                  </button>
                   <button className="btn btn-neutral">Listen</button>
                 </div>
               </div>
@@ -51,7 +58,7 @@ export default async function Episode({ params }: Props) {
           </header>
           <hr className="my-12 border-gray-200" />
           <div
-            className="prose prose-slate mt-14 flex flex-col gap-3 [&>h2:nth-of-type(3n)]:before:bg-violet-200 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5"
+            className="prose prose-slate mt-14 [&>h2:nth-of-type(3n)]:before:bg-violet-200 [&>h2:nth-of-type(3n+2)]:before:bg-indigo-200 [&>h2]:mt-12 [&>h2]:flex [&>h2]:items-center [&>h2]:font-mono [&>h2]:text-sm [&>h2]:font-medium [&>h2]:leading-7 [&>h2]:text-slate-900 [&>h2]:before:mr-3 [&>h2]:before:h-3 [&>h2]:before:w-1.5 [&>h2]:before:rounded-r-full [&>h2]:before:bg-cyan-200 [&>ul]:mt-6 [&>ul]:list-['\2013\20'] [&>ul]:pl-5"
             dangerouslySetInnerHTML={{ __html: data.content }}
           />
         </Container>
